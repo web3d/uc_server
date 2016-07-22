3d6d5631e63c76d74c8fb5ff1a360ff6bae4ab4e
web3d <web3d@live.com>
2016-07-22 23:06:11 +0800

    Mail模型,中sendmail片段引用方式改为Mailer;整理代码的写法,基于PHP5

--- app/model/Mail.php
+++ app/model/Mail.php
@@ -2,34 +2,33 @@
 
 namespace uc\server\app\model;
 
-define('UC_MAIL_REPEAT', 5);
-
 class Mail
 {
+    const UC_MAIL_REPEAT = 5;
 
-    var $db;
+    protected $db;
 
-    var $base;
+    protected $base;
 
-    var $apps;
+    protected $apps;
 
-    function __construct(&$base)
+    public function __construct(&$base)
     {
         $this->base = $base;
         $this->db = $base->db;
         $this->apps = &$this->base->cache['apps'];
     }
 
-    function get_total_num()
+    public function get_total_num()
     {
-        $data = $this->db->result_first("SELECT COUNT(*) FROM " . UC_DBTABLEPRE . "mailqueue");
+        $data = $this->db->result_first("SELECT COUNT(*) FROM {{%mailqueue}}");
         return $data;
     }
 
-    function get_list($page, $ppp, $totalnum)
+    public function get_list($page, $ppp, $totalnum)
     {
         $start = $this->base->page_get_start($page, $ppp, $totalnum);
-        $data = $this->db->fetch_all("SELECT m.*, u.username, u.email FROM " . UC_DBTABLEPRE . "mailqueue m LEFT JOIN " . UC_DBTABLEPRE . "members u ON m.touid=u.uid ORDER BY dateline DESC LIMIT $start, $ppp");
+        $data = $this->db->fetch_all("SELECT m.*, u.username, u.email FROM {{%mailqueue}} m LEFT JOIN {{%members}} u ON m.touid=u.uid ORDER BY dateline DESC LIMIT $start, $ppp");
         foreach ((array) $data as $k => $v) {
             $data[$k]['subject'] = dhtmlspecialchars($v['subject']);
             $data[$k]['tomail'] = empty($v['tomail']) ? $v['email'] : $v['tomail'];
@@ -39,32 +38,37 @@ class Mail
         return $data;
     }
 
-    function delete_mail($ids)
+    /**
+     * 
+     * @param array $ids
+     * @return int
+     */
+    public function delete_mail(array $ids)
     {
         $ids = $this->base->implode($ids);
-        $this->db->query("DELETE FROM " . UC_DBTABLEPRE . "mailqueue WHERE mailid IN ($ids)");
+        $this->db->query("DELETE FROM {{%mailqueue}} WHERE mailid IN ($ids)");
         return $this->db->affected_rows();
     }
 
-    function add($mail)
+    public function add(array $mail)
     {
         if ($mail['level']) {
-            $sql = "INSERT INTO " . UC_DBTABLEPRE . "mailqueue (touid, tomail, subject, message, frommail, charset, htmlon, level, dateline, failures, appid) VALUES ";
+            $sql = "INSERT INTO {{%mailqueue}} (touid, tomail, subject, message, frommail, charset, htmlon, level, dateline, failures, appid) VALUES ";
             $values_arr = array();
             foreach ($mail['uids'] as $uid) {
                 if (empty($uid))
                     continue;
-                $values_arr[] = "('$uid', '', '$mail[subject]', '$mail[message]', '$mail[frommail]', '$mail[charset]', '$mail[htmlon]', '$mail[level]', '$mail[dateline]', '0', '$mail[appid]')";
+                $values_arr[] = "('$uid', '', '{$mail['subject']}', '{$mail['message']}', '{$mail['frommail']}', '{$mail['charset']}', '{$mail['htmlon']}', '{$mail['level']}', '{$mail['dateline']}', '0', '{$mail['appid']}')";
             }
             foreach ($mail['emails'] as $email) {
                 if (empty($email))
                     continue;
-                $values_arr[] = "('', '$email', '$mail[subject]', '$mail[message]', '$mail[frommail]', '$mail[charset]', '$mail[htmlon]', '$mail[level]', '$mail[dateline]', '0', '$mail[appid]')";
+                $values_arr[] = "('', '$email', '{$mail['subject']}', '{$mail['message']}', '{$mail['frommail']}', '{$mail['charset']}', '{$mail['htmlon']}', '{$mail['level']}', '{$mail['dateline']}', '0', '{$mail['appid']}')";
             }
             $sql .= implode(',', $values_arr);
             $this->db->query($sql);
             $insert_id = $this->db->insert_id();
-            $insert_id && $this->db->query("REPLACE INTO " . UC_DBTABLEPRE . "vars SET name='mailexists', value='1'");
+            $insert_id && $this->db->query("REPLACE INTO {{%vars}} SET name='mailexists', value='1'");
             return $insert_id;
         } else {
             $mail['email_to'] = array();
@@ -74,7 +78,7 @@ class Mail
                     continue;
                 $uids .= ',' . $uid;
             }
-            $users = $this->db->fetch_all("SELECT uid, username, email FROM " . UC_DBTABLEPRE . "members WHERE uid IN ($uids)");
+            $users = $this->db->fetch_all("SELECT uid, username, email FROM {{%members}} WHERE uid IN ($uids)");
             foreach ($users as $v) {
                 $mail['email_to'][] = $v['username'] . '<' . $v['email'] . '>';
             }
@@ -89,7 +93,7 @@ class Mail
         }
     }
 
-    function send()
+    public function send()
     {
         register_shutdown_function(array(
             $this,
@@ -97,11 +101,11 @@ class Mail
         ));
     }
 
-    function _send()
+    private function _send()
     {
         $mail = $this->_get_mail();
         if (empty($mail)) {
-            $this->db->query("REPLACE INTO " . UC_DBTABLEPRE . "vars SET name='mailexists', value='0'");
+            $this->db->query("REPLACE INTO {{%vars}} SET name='mailexists', value='0'");
             return NULL;
         } else {
             $mail['email_to'] = $mail['tomail'] ? $mail['tomail'] : $mail['username'] . '<' . $mail['email'] . '>';
@@ -115,7 +119,7 @@ class Mail
         }
     }
 
-    function send_by_id($mailid)
+    public function send_by_id($mailid)
     {
         if ($this->send_one_mail($this->_get_mail_by_id($mailid))) {
             $this->_delete_one_mail($mailid);
@@ -123,36 +127,36 @@ class Mail
         }
     }
 
-    function send_one_mail($mail)
+    public function send_one_mail(array $mail)
     {
         if (empty($mail))
             return;
         $mail['email_to'] = $mail['email_to'] ? $mail['email_to'] : $mail['username'] . '<' . $mail['email'] . '>';
-        $mail_setting = $this->base->settings;
-        return include UC_ROOT . 'lib/sendmail.inc.php';
+        return (new \uc\server\Mailer($this->base->settings))
+                ->send($mail, $this->base->cache['apps'][$mail['appid']]['name']);
     }
 
-    function _get_mail()
+    private function _get_mail()
     {
-        $data = $this->db->fetch_first("SELECT m.*, u.username, u.email FROM " . UC_DBTABLEPRE . "mailqueue m LEFT JOIN " . UC_DBTABLEPRE . "members u ON m.touid=u.uid WHERE failures<'" . UC_MAIL_REPEAT . "' ORDER BY level DESC, mailid ASC LIMIT 1");
+        $data = $this->db->fetch_first("SELECT m.*, u.username, u.email FROM {{%mailqueue}} m LEFT JOIN {{%members}} u ON m.touid=u.uid WHERE failures<'" . UC_MAIL_REPEAT . "' ORDER BY level DESC, mailid ASC LIMIT 1");
         return $data;
     }
 
-    function _get_mail_by_id($mailid)
+    private function _get_mail_by_id($mailid)
     {
-        $data = $this->db->fetch_first("SELECT m.*, u.username, u.email FROM " . UC_DBTABLEPRE . "mailqueue m LEFT JOIN " . UC_DBTABLEPRE . "members u ON m.touid=u.uid WHERE mailid='$mailid'");
+        $data = $this->db->fetch_first("SELECT m.*, u.username, u.email FROM {{%mailqueue}} m LEFT JOIN {{%members}} u ON m.touid=u.uid WHERE mailid='$mailid'");
         return $data;
     }
 
-    function _delete_one_mail($mailid)
+    private function _delete_one_mail($mailid)
     {
         $mailid = intval($mailid);
-        return $this->db->query("DELETE FROM " . UC_DBTABLEPRE . "mailqueue WHERE mailid='$mailid'");
+        return $this->db->query("DELETE FROM {{%mailqueue}} WHERE mailid='$mailid'");
     }
 
-    function _update_failures($mailid)
+    private function _update_failures($mailid)
     {
         $mailid = intval($mailid);
-        return $this->db->query("UPDATE " . UC_DBTABLEPRE . "mailqueue SET failures=failures+1 WHERE mailid='$mailid'");
+        return $this->db->query("UPDATE {{%mailqueue}} SET failures=failures+1 WHERE mailid='$mailid'");
     }
 }
