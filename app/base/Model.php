7c9b189fa41274472774fc5d1f589cbf74771e2c
web3d <web3d@live.com>
2016-07-23 03:09:43 +0800

    增加简单的model基类,主要用于简化 原有model类构造函数重复内容的问题

---
+++ app/base/Model.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace uc\server\app\base;
+
+class Model
+{
+    
+    /**
+     *
+     * @var \uc\server\db\DBInterface db引擎对象
+     */
+    protected $db;
+
+    /**
+     *
+     * @var Control 系统原来的关系,引用控制器
+     */
+    protected $base;
+    
+    /**
+     *
+     * @var string 表名 {{%table_name}}
+     */
+    protected $tableName;
+
+    /**
+     * 构造函数
+     * @param \uc\server\app\base\Control $base 引用控制器
+     */
+    public function __construct(Control &$base)
+    {
+        $this->base = $base;
+        $this->db = $base->db;
+    }
+}
